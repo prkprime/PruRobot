c31f93a7732b905d9a92bcca4f1969e1bb119501
Pratik Gorade <pratikgorade0@gmail.com>
2021-03-16 14:17:50 +0530

    minor change
    Signed-off-by: Pratik Gorade <pratikgorade0@gmail.com>

--- bot/modules/inline_ket.py
+++ bot/modules/inline_ket.py
@@ -11,18 +11,19 @@ def inline_ket(update, context):
     query = query.split(' ', 1)
     if len(query) < 2:
         context.bot.answer_inline_query(update.inline_query.id, [inline_welp])
-    results = [
-        InlineQueryResultPhoto(
-            id=uuid4(),
-            type='photo',
-            title=i,
-            photo_url=f'https://http.cat/{i}',
-            thumb_url=f'https://http.cat/{i}',
-            caption=i,
-        )
-        for i in http_code_list
-        if i.startswith(query[1])
-    ]
+    else:
+        results = [
+            InlineQueryResultPhoto(
+                id=uuid4(),
+                type='photo',
+                title=i,
+                photo_url=f'https://http.cat/{i}',
+                thumb_url=f'https://http.cat/{i}',
+                caption=i,
+            )
+            for i in http_code_list
+            if i.startswith(query[1])
+        ]
     if not results:
         context.bot.answer_inline_query(update.inline_query.id, [inline_welp])
     context.bot.answer_inline_query(update.inline_query.id, results)
