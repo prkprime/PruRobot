7e9d7c38c170339dbf25d13d4868d0ccea744a12
Pratik Gorade <pratikgorade0@gmail.com>
2021-03-16 13:30:31 +0530

    eek
    Signed-off-by: Pratik Gorade <pratikgorade0@gmail.com>

--- bot/modules/assets/help_message.py
+++ bot/modules/assets/help_message.py
@@ -1,15 +1,15 @@
 from telegram import InlineQueryResultArticle, InputTextMessageContent
 from uuid import uuid4
 
-inline_welp_message = """Hello There.\n
+inline_welp_message = '''Hello There.\n
 @PruRobot ket <http status code>\n
 @PruRobot dogge <http status code>\n
 
 Kode l1nk : https://github.com/prkprime/PruRobot
-"""
+'''
 
 inline_welp = InlineQueryResultArticle(
     id=uuid4(),
-    title="Click here for Welp",
+    title='Click here for Welp',
     input_message_content=InputTextMessageContent(message_text=inline_welp_message),
 )
