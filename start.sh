00f5fcf5529ddcba16052f9d34d5a376255a0809
Pratik Gorade <pratikgorade0@gmail.com>
2022-09-01 13:00:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # shellcheck disable=SC1091
 
-[[ -d "venv" ]] || python3.8 -m venv ./venv
+[[ -d "venv" ]] || python3 -m venv ./venv
 source venv/bin/activate
 pip install -U -r requirements.txt
 python3 -m bot
